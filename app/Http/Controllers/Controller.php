83583327620b3ad937e84a1ccff419e38804d895
khanmdsagar <khanmdsagar96@gmail.com>
2020-05-28 11:20:04 +0600

    select update delete and search functionality added

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -8,6 +8,12 @@ use Laravel\Lumen\Routing\Controller as BaseController;
 
 class Controller extends BaseController
 {
+
+    function home(){
+        return "Lumen Phone Book Api";
+    }
+
+
     function insertData(Request $request){
         $name    =  $request->input('name');
         $phone   =  $request->input('phone');
@@ -23,4 +29,55 @@ class Controller extends BaseController
             return "Insert Failed";
         }
     }
+
+
+    function selectData(){
+        $selectData = peoples::orderBy('name', 'ASC')->get();
+        return $selectData;
+    }
+
+
+    function updateData(Request $request, $id){
+
+        $name   = $request->input('name');
+        $phone   = $request->input('phone');
+        $email  = $request->input('email');
+ 
+        //DB::table('students')
+        $result = peoples::where('id', $id) -> update(['name'=> $name, 'phone'=> $phone, 'email'=> $email]);
+ 
+       
+        if($result == true){
+            return 'Updated Successfully';
+        }
+        else{
+            return 'Update Failed';
+        }
+
+    }
+
+
+    function deleteData($id){
+
+        $result = peoples::where('id', $id)->delete();
+ 
+        if($result == true){
+            return 'Deleted Successfully';
+        }
+        else{
+            return 'Delete Failed';
+        }
+    }
+
+
+    function searchData($name){
+    
+      $result = peoples::orderBy('name', 'ASC')
+        ->where('name', 'LIKE', "%{$name}%")
+        ->get();
+      return $result;
+
+    }
+
+
 }
--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,14 @@
 <?php
 
-$router->get('/insertData', 'Controller@insertData');
+$router->get('/', 'Controller@home');
+
+$router->post('/insertData', 'Controller@insertData');
+
+$router->get('/selectData', 'Controller@selectData');
+
+$router->post('/updateData/{id}', 'Controller@updateData');
+
+$router->post('/deleteData/{id}', 'Controller@deleteData');
+
+$router->get('/searchData/{name}', 'Controller@searchData');
 
